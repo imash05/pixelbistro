8ecd9d0366373ca88b3dff319a9c75a86a067a2e
IFer <76787367+imash05@users.noreply.github.com>
2024-01-15 23:12:21 +0100

    ..

---
+++ src/widgets/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const logo = require('/src/assets/logo/logo.png')
+
+const Navbar = () => {
+  return (
+    <div >
+        
+        <dvi><img src={logo} alt='logo' /> </dvi>
+        <dvi></dvi>
+        <dvi></dvi>
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar
